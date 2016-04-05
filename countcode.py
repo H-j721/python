c0af0584d9427d99934d219f6041faf10c3d8c4f
H-j721 <clr1024@163.com>
2016-04-05 16:21:37 +0800

    make it crystal clear

--- countcode.py
+++ countcode.py
@@ -4,44 +4,60 @@ __author__ = 'JiangHE'
 
 '''
 Count your code 
+Default: Count py files in current dir(if not assign filetype and dir)
 Usage: countcode.py filetype dir
 '''
 
 # Create time   : 2016-03-29 15:13:11
-# Last modified : 2016-04-05 14:08:22
+# Last modified : 2016-04-05 16:18:54
 #########################################################
 
 import os, sys
 
 class CountCode:
-	def __init__(self, line=0, filetype='py', dirs=os.chdir(os.getcwd())):
+	def __init__(self, line=0, filetype='py', dirs=os.getcwd()):
 		self.line = line
 		self.filetype = filetype
 		self.dirs = dirs
-	def lister(self):
+
+	# Including subdirectories
+	def recurCount(self):
 		fnum = 0
-		print self.dirs
-#		os.chdir(self.dirs)
 		tlen = len(self.filetype)
+
+		try:
+			subdirs = os.listdir(self.dirs)
+		except WindowsError:
+			print 'Please check your directory name !'
+			sys.exit()
+
+		print '[',self.dirs,
+		print "including %d subdirectories" % len(subdirs),']'
 		for (thisdir, subshere, fileshere) in os.walk(self.dirs):
-#			print '['+thisdir+']'
 			for fname in fileshere:
 				path = os.path.join(thisdir, fname)
-				bname = os.path.basename(path)
-#				print path
-				if bname[-tlen:]==self.filetype:
+#				bname = os.path.basename(path)
+#				if bname[-tlen:]==self.filetype:
+				if path[-tlen:]==self.filetype:
 					fnum += 1
 					file = open(path)
 					while(file.readline()):
 						self.line += 1
 					file.close()
-		print 'There are %d %s files in %s' % (fnum, self.filetype, self.dirs)
+		print 'There are %d %s files' % (fnum, self.filetype)
 		print "And %d lines code there, including comments" % self.line
 			
-	def count(self):
-		L = os.listdir(self.dirs)
+	# Only Traverse current directory
+	def directCount(self):
+		tlen = len(self.filetype)
+		try:
+			L = os.listdir(self.dirs)
+		except WindowsError:
+			print 'Please check your directory name !'
+			sys.exit()
+		print '[',self.dirs,']'
 		filelist = [f for f in L if f[-tlen:]==self.filetype]
-		print 'There are %d %s files in %s' % (len(filelist), self.filetype, self.dirs)
+		print 'There are %d %s files' % (len(filelist), self.filetype)
 		os.chdir(self.dirs)
 		for f in filelist:
 			file = open(f)
@@ -50,8 +66,15 @@ class CountCode:
 			file.close()
 		print "And %d lines code there, including comments" % self.line
 
+
 if __name__ == '__main__':
-	ft = sys.argv[1]
-	fd = sys.argv[2]
-	count = CountCode(filetype=ft, dirs=fd)
-	count.lister()
+	if len(sys.argv) > 2:
+		ft = sys.argv[1]
+		fd = sys.argv[2]
+		count = CountCode(filetype=ft, dirs=fd)
+	else:
+		count = CountCode()
+
+	count.recurCount()
+	count.line = 0
+	count.directCount()
