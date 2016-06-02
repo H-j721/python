3f232c250e50f98e36f7e7e71b69fc4309b67149
H-j721 <clr1024@163.com>
2016-06-03 01:25:20 +0800

    find longest palindrome

--- palindrome.py
+++ palindrome.py
@@ -6,13 +6,14 @@ __author__ = 'JiangHE'
 longest Palindrome
 '''
 
-
 # Create time   : 2016-04-03 23:21
 # Last modified : 2016-04-03 23:27
 #########################################################
+
 # RL回文半径数组: RL[i]表示以第i个字符为对称轴的回文串的回文半径
 # maxRight: 当前访问到的所有回文子串,所能触及的最右一个字符的位置
 # pos: MaxRight对应的回文串的对称轴所在的位置
+
 def manacher(s):
     s = '#' + '#'.join(s) + '#'
 
